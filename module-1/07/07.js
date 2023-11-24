e373e9ac0a605b86940789ed78e7d327dd0892a1
Sheng.T <alxt9@outlook.com>
2023-11-24 22:22:26 +0200

    variable name change

--- module-1/07/07.js
+++ module-1/07/07.js
@@ -2,14 +2,14 @@ const count = parseInt(prompt("How many dices?"))
 if (isNaN(count)) {
     document.querySelector("#dice").innerHTML = "Please enter a valid number."
 } else {
-    let num
-    let sum = 0
+    let dice
     let dices = ""
+    let sum = 0
 
     for (let i = 0; i < count; i++) {
-        num = Math.floor(Math.random() * 6 + 1)
-        sum += num
-        dices = `${dices} ${num}`
+        dice = Math.floor(Math.random() * 6 + 1)
+        sum += dice
+        dices = `${dices} ${dice}`
     }
 
     document.querySelector("#dice").innerHTML = `Rolled ${count} dices: ${dices}`
