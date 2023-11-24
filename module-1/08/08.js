fb63e234bcc5fd265359d2540b376709295b01fd
Sheng.T <alxt9@outlook.com>
2023-11-24 22:29:22 +0200

    connect string before update html file

--- module-1/08/08.js
+++ module-1/08/08.js
@@ -4,9 +4,12 @@ if (isNaN(startYear) || isNaN(endYear)) {
     document.querySelector("#title").innerHTML = "Please enter valid years."
 } else {
     document.querySelector("#title").innerHTML = `Leap years from ${startYear} to ${endYear}:`
+    let list = ""
     for (let year = startYear; year <= endYear; year++) {
         if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-            document.querySelector("#list").innerHTML += `<li>${year}</li>`
+            list += `<li>${year}</li>`
         }
     }
+    document.querySelector("#list").innerHTML = list
+
 }
---
+++ module-2/01/01.js
@@ -0,0 +1,12 @@
+numbers = []
+
+for (let i = 0; i <= 5; i++) {
+    numbers[i] = parseFloat(prompt("Enter a number:"))
+}
+
+for (let i = 0; i <= 5; i++) {
+    numbers[i] = parseFloat(prompt("Enter a number:"))
+}
+
+
+document.querySelector("#title").innerHTML = `Simulated times: ${simulateCount}`
